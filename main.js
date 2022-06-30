47670fadf8facd7e378a8512e52c46023675cb8f
Madhuri G <madhurik.ganta@gmail.com>
2022-06-30 08:55:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = ml5.imageClassier('mobileNet')
+}
+
+function modelLoaded(){
+  console.log('Model Loaded!');
+}
+
+
+
